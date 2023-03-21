b758669213eab900d54b7419eda5ab887448ea66
Yuh-Boh Feng <fengyuhboh@gmail.com>
2023-03-20 17:54:04 -0700

    finished question 5 in object-practice.js

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -55,3 +55,6 @@ console.log(me.shoeCount);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+console.log('5.');
+me.favoriteColor = "Blellow";
+console.log(me);
